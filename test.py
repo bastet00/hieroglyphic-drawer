d385382cdaf2a0f88eb04eded473f216b447265a
Yossef-Elshafey <yssef0472@gmail.com>
2024-12-14 23:45:27 +0200

    test file corrections

--- test.py
+++ test.py
@@ -1,20 +1,8 @@
 import unittest
-from read import MdcConvertor
+from handlers.convertor import MdcConvertor
 
 
-class MdcConverterTest(unittest.TestCase):
-    def test_convert_mdc_to_unicode__should_return_right_unicode(self):
-        mdcConverter = MdcConverter("A1-B1")
-        hieroglyphics_unicode = mdcConverter.to_unicode()
-        self.self.assertEqual(hieroglyphics_unicode, "𓀀𓁐")
-
-    def test_convert_mdc_to_image(self):
-        mdcConverter = MdcConverter("A1-B1")
-        hieroglyphics_unicode = mdcConverter.to_image(size=(100, 100))
-        # assert image is created or so
-
-
-class BlockTest(unittest.TestCase):
+class ConvertorTest(unittest.TestCase):
     """
     Test drawing types to the input
     """
@@ -30,15 +18,15 @@ class BlockTest(unittest.TestCase):
         blocks = generator.unicode_to_draw_array()
         self.assertEqual(blocks[0]["draw_type"], "compose")
 
-    def test_raise_key_error(self):
-        generator = MdcConvertor("./signs_mapper.json", "Z100")
-        with self.assertRaises(KeyError):
-            generator.unicode_to_draw_array()
-
-    def test_vertical_draw_block(self):
+    def test_vertical_block(self):
         generator = MdcConvertor("./signs_mapper.json", "F40:X1:F34")
         blocks = generator.unicode_to_draw_array()
-        self.assertEqual(blocks[0]["draw_type"], "vertical_draw")
+        self.assertEqual(blocks[0]["draw_type"], "vertical")
+
+    def test_compose_up_block(self):
+        generator = MdcConvertor("./signs_mapper.json", "F40*X1:F34")
+        blocks = generator.unicode_to_draw_array()
+        self.assertEqual(blocks[0]["draw_type"], "compose_up")
 
     def test_multiple_block(self):
         generator = MdcConvertor(
@@ -50,7 +38,7 @@ class BlockTest(unittest.TestCase):
             self.assertEqual(block["draw_type"], "stand_alone")
         vertical_draw = blocks[2:4]
         for block in vertical_draw:
-            self.assertEqual(block["draw_type"], "vertical_draw")
+            self.assertEqual(block["draw_type"], "vertical")
         compose = blocks[-1]
         self.assertEqual(compose["draw_type"], "compose")
 
